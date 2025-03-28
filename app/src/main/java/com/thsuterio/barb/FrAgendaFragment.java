53dc9a79d0bd2906715b501d91ab833feea58b9b
Tiraz <hugo.suterio@hotmail.com>
2025-03-28 20:37:22 -0300

    TEstando calendario

--- app/src/main/java/com/thsuterio/barb/FrAgendaFragment.java
+++ app/src/main/java/com/thsuterio/barb/FrAgendaFragment.java
@@ -1,7 +1,9 @@
 package com.thsuterio.barb;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,7 +12,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
 import android.widget.Toast;
+
+import com.google.android.material.datepicker.MaterialDatePicker;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,14 +27,30 @@ public class FrAgendaFragment extends Fragment implements DialogAgendarInputFlag
     List<ObjAgendado> list_agendado = new ArrayList<>();
     Button btn_novo_agendamento;
 
+
+    CalendarView calendarView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_fr_agenda, container, false);
 
+        //Apresentação de variaveis
         recyclerView = view.findViewById(R.id.recFragmentAgenda);
-
         btn_novo_agendamento = view.findViewById(R.id.btnNovoAgendamento);
+        calendarView = view.findViewById(R.id.dtpAgenda);
+
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                int ajuste_mes = i1 + 1;
+                Toast.makeText(getContext(), " data: " + i2 + "/" + ajuste_mes + "/" + i, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
         btn_novo_agendamento.setOnClickListener(v -> {
             DialogAgendarInputFlagment dialog = new DialogAgendarInputFlagment();
             dialog.show(getChildFragmentManager(), "Teste");
